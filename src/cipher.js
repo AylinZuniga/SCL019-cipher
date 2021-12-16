0a683dd5ada1bc8b8efb66a1d8465b20555b9b06
Aylin Zuan <aylinzuan@Eduardos-MacBook-Pro.local>
2021-12-16 08:56:09 -0300

    primer guardado

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,74 @@
 const cipher = {
-  // ...
+
+  cifrado: function (texto,rango) {
+  
+    var fraseCifrada='' ;
+    var textoOriginal= texto.toUpperCase() ;    
+    
+    for (let index = 0; index < textoOriginal.length; index++) {
+        const letra = textoOriginal[index];
+       
+        if(textoOriginal[index] == ' ')
+        {
+            fraseCifrada = fraseCifrada + ' '
+        }
+
+        else if (textoOriginal[index] == ',')
+          {
+            fraseCifrada = fraseCifrada + ','
+
+          }
+        
+        else
+        { 
+            var letraCifrada=  String.fromCharCode(((letra.charCodeAt() - 65 + parseInt(rango)) % 26 )+ 65)  
+            fraseCifrada = fraseCifrada + letraCifrada 
+        }
+        
+    }
+     
+    return fraseCifrada;
+
+    }
+    ,
+
+
+    descifrado: function (texto,rango) {
+  
+      var fraseCifrada='' ;
+      var textoOriginal= texto.toUpperCase() ;    
+      
+      for (let index = 0; index < textoOriginal.length; index++) {
+          const letra = textoOriginal[index];
+         
+          if(textoOriginal[index] == ' ')
+          {
+              fraseCifrada = fraseCifrada + ' '
+          }
+  
+          else if (textoOriginal[index] == ',')
+          {
+            fraseCifrada = fraseCifrada + ','
+
+          }
+
+          else
+          { 
+              
+              var letraCifrada= String.fromCharCode((letra.charCodeAt() + 65 - parseInt (rango)) % 26+65)
+
+              fraseCifrada = fraseCifrada + letraCifrada 
+
+
+            
+          }
+          
+      }
+       
+      return fraseCifrada;
+  
+      }
+    
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+    document.getElementById('btncipher').addEventListener("click", function(){
+        
+        var inicialText= document.getElementById('mainText').value;
+        var range= document.getElementById('rango').value;
+        var textoCifrado= cipher.cifrado (inicialText,range)
+    
+        var cipherMessage= document.getElementById('finalMesagge');
+        cipherMessage.innerHTML= textoCifrado;         
+    });
+
+    
+    document.getElementById('btnDecipher').addEventListener("click", function(){
+        
+        var inicialText= document.getElementById('mainText').value;
+        var range= document.getElementById('rango').value;
+        var textoCifrado= cipher.descifrado (inicialText,range)
+    
+        var cipherMessage= document.getElementById('finalMesagge');
+        cipherMessage.innerHTML= textoCifrado;         
+    });
+
+
+
+    document.getElementById('btnSend').addEventListener("click", function(){
+        document.getElementById('finalMesagge').value = "";
+        document.getElementById('mainText').value = "";          
+    });
+
+
+    document.getElementById('mainText').addEventListener( '', function(){
+        document.getElementById('finalMesagge').value = "";
+              
+    });
+
+
+    document.getElementById('validar').addEventListener("click", function(event){
+     event.preventDefault();     
+     var usuario = document.getElementById('name').value
+     var contrasena = document.getElementById('pass').value
+
+     if(usuario == '13' && contrasena== '12345')
+     {
+         alert('usuario valido')
+
+       document.getElementById('formulario').style.display= 'none'
+       document.getElementById('cifrador').style.display= 'block'
+      
+     }
+
+     else{ 
+         alert ('verifique sus datos')
+     }
+    });
+
+   
